c79cf33159c638fe3e44cf207ff4bcba11f94189
lionel5116 <lioneljones5116@gmail.com>
2020-05-08 11:25:51 -0500

    more code for the component

--- force-app/main/default/lwc/productTile/productTile.js
+++ force-app/main/default/lwc/productTile/productTile.js
@@ -1,10 +1,30 @@
-import { LightningElement } from 'lwc';
+import { LightningElement,api,track } from 'lwc';
 
 export default class ProductTile extends LightningElement {
+    _product;
 
+    @track pictureURL;
+    @track name;
+    @track msrp;
+
+
+    @api
+    get product()
+    {
+        return this._product;
+    }
+
+   set product(value)
+   {
+    this._product = value;
+    this.pictureURL = value.Picture_URL__c;
+    this.name=value.name;
+    this.msrp = MSRP__c;
+   }
 
     handleClick()
     {
-        
+       const selectedEvent =  new CustomEvent('selected',{detail:this.product.id});
+       this.dispatchEvent(selectedEvent);
     }
 }
