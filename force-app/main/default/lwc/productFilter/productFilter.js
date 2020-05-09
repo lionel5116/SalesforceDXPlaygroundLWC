af3a5e5d8f16c01edea202b39166f3f8ea5a55e5
lionel5116 <lioneljones5116@gmail.com>
2020-05-09 11:07:47 -0500

    more code for the component

---
+++ force-app/main/default/lwc/productFilter/productFilter.js
@@ -0,0 +1,19 @@
+import { LightningElement,api,track } from 'lwc';
+
+export default class ProductFilter extends LightningElement {
+
+    handleSearchKeyChange()
+    {
+
+    }
+
+    handleMaxPriceChange() {
+
+    }
+
+    handleCheckBoxChange()
+    {
+
+    }
+
+}
