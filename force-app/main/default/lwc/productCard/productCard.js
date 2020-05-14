d41a7ee6f45cdb16592b3192e5fba8338b950673
lionel5116 <lioneljones5116@gmail.com>
2020-05-14 11:39:23 -0500

    more code for product CArd

--- force-app/main/default/lwc/productCard/productCard.js
+++ force-app/main/default/lwc/productCard/productCard.js
@@ -1,3 +1,42 @@
-import { LightningElement } from 'lwc';
+import { LightningElement,wire } from 'lwc';
+import {CurrentPageReference} from 'lightning/navigation';
+import {getRecord} from 'lightning/uiRecordApi';
+import {registerListener, unregisterAllListers} from 'c/pubsub';
 
-export default class ProductCard extends LightningElement {}
+import PRODUCT_OBJECT from '@salesforce/schema/Product__c';
+import NAME_FEILD from '@salesforce/schema/Product__c.Name';
+import LEVEL_FIELD from '@salesforce/schema/Product__c.Level__c';
+import CATEGORY_FIELD from '@salesforce/schema/Product__c.Category__c';
+import MATERIAL_FIELD from '@salesforce/schema/Product__c.Material__c';
+import MSRP_FIELD from '@salesforce/schema/Product__c.MSRP__c';
+import PICTURE_URL_FIELD from '@salesforce/schema/Product__c.Picutre_URL__c';
+
+const fields=[
+    NAME_FEILD,
+    LEVEL_FIELD,
+    CATEGORY_FIELD,
+    MATERIAL_FIELD,
+    MSRP_FIELD,
+    PICTURE_URL_FIELD
+  ]
+
+export default class ProductCard extends LightningElement {
+   recordId;
+   @wire(CurrentPageReference) pageRef;
+   @wire(getRecord, {recordId:'$recordId',fields})
+
+   connectedCallback()
+   {
+       registerListener('productSelected',this.handleProductSelected,this);
+   }
+
+   disconnectedCallback()
+   {
+    unregisterAllListers(this);
+   }
+
+   handleProductSelected(productId)
+   {
+      this.recordId = productId;
+   }
+}
