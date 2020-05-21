aecab4ad89150876c4673258d1ce9037edf06672
lionel5116 <lioneljones5116@gmail.com>
2020-05-21 13:05:11 -0500

    more code

--- force-app/main/default/lwc/productCard/productCard.js
+++ force-app/main/default/lwc/productCard/productCard.js
@@ -1,48 +1,73 @@
-import { LightningElement,wire } from 'lwc';
-import {CurrentPageReference} from 'lightning/navigation';
-import {getRecord} from 'lightning/uiRecordApi';
-import {registerListener, unregisterAllListers} from 'c/pubsub';
+import { LightningElement, wire } from 'lwc';
+import { CurrentPageReference } from 'lightning/navigation';
+import { NavigationMixin } from 'lightning/navigation';
 
+/** Wire adapter to load records, utils to extract values. */
+import { getRecord } from 'lightning/uiRecordApi';
+
+/** Pub-sub mechanism for sibling component communication. */
+import { registerListener, unregisterAllListeners } from 'c/pubsub';
+
+/** Product__c Schema. */
 import PRODUCT_OBJECT from '@salesforce/schema/Product__c';
-import NAME_FEILD from '@salesforce/schema/Product__c.Name';
+import NAME_FIELD from '@salesforce/schema/Product__c.Name';
 import LEVEL_FIELD from '@salesforce/schema/Product__c.Level__c';
 import CATEGORY_FIELD from '@salesforce/schema/Product__c.Category__c';
 import MATERIAL_FIELD from '@salesforce/schema/Product__c.Material__c';
 import MSRP_FIELD from '@salesforce/schema/Product__c.MSRP__c';
-import PICTURE_URL_FIELD from '@salesforce/schema/Product__c.Picutre_URL__c';
+import PICTURE_URL_FIELD from '@salesforce/schema/Product__c.Picture_URL__c';
 
-const fields=[
-    NAME_FEILD,
+/** Record fields to load. */
+const fields = [
+    NAME_FIELD,
     LEVEL_FIELD,
     CATEGORY_FIELD,
     MATERIAL_FIELD,
     MSRP_FIELD,
     PICTURE_URL_FIELD
-  ]
+];
 
-export default class ProductCard extends LightningElement {
-   recordId;
-   @wire(CurrentPageReference) pageRef;
-   @wire(getRecord, {recordId:'$recordId',fields})
-   product;
+/**
+ * Component to display details of a Product__c.
+ */
+export default class ProductCard extends NavigationMixin(LightningElement) {
+    /** Id of Product__c to display. */
+    recordId;
 
-   connectedCallback()
-   {
-       registerListener('productSelected',this.handleProductSelected,this);
-   }
+    @wire(CurrentPageReference) pageRef;
 
-   disconnectedCallback()
-   {
-    unregisterAllListers(this);
-   }
+    /** Load the Product__c to display. */
+    @wire(getRecord, { recordId: '$recordId', fields })
+    product;
 
-   handleProductSelected(productId)
-   {
-      this.recordId = productId;
-   }
+    connectedCallback() {
+        registerListener('productSelected', this.handleProductSelected, this);
+    }
 
-   get noData()
-   {
-      return !this.product.error && !this.product.data;
-   }
+    disconnectedCallback() {
+        unregisterAllListeners(this);
+    }
+
+    /**
+     * Handler for when a product is selected. When `this.recordId` changes, the @wire
+     * above will detect the change and provision new data.
+     */
+    handleProductSelected(productId) {
+        this.recordId = productId;
+    }
+
+    handleNavigateToRecord() {
+        this[NavigationMixin.Navigate]({
+            type: 'standard__recordPage',
+            attributes: {
+                recordId: this.recordId,
+                objectApiName: PRODUCT_OBJECT.objectApiName,
+                actionName: 'view'
+            }
+        });
+    }
+
+    get noData() {
+        return !this.product.error && !this.product.data;
+    }
 }
--- force-app/main/default/lwc/productTileList/productTileList.js
+++ force-app/main/default/lwc/productTileList/productTileList.js
@@ -1,44 +1,77 @@
-import { LightningElement, api,track,wire } from 'lwc';
-import {CurrentPageReference} from 'lightning/navigation';
-import getProducts from '@salesforce/apex/ProductsController.getProducts';
-import {registerListener,unregisterAllListeners,fireEvent} from 'c/pubsub';
+import { LightningElement, api, track, wire } from 'lwc';
+import { CurrentPageReference } from 'lightning/navigation';
 
+/** getProducts() method in ProductController Apex class */
+import getProducts from '@salesforce/apex/ProductController.getProducts';
+
+/** Pub-sub mechanism for sibling component communication. */
+import { registerListener, unregisterAllListeners, fireEvent } from 'c/pubsub';
+
+/**
+ * Container component that loads and displays a list of Product__c records.
+ */
 export default class ProductTileList extends LightningElement {
-    @track pageNumber=1;
-    @track pageSize;
-    @track totalItemCount=0;
-    @track filters={};
-    @wire(CurrentPageReference) pageRef;
-    @wire(getProducts,{filters:'$filters',pageNumber:'$pageNumber'}) products;
+    /**
+     * Whether to display the search bar.
+     * TODO - normalize value because it may come as a boolean, string or otherwise.
+     */
+    @api searchBarIsVisible = false;
 
-    connectedCallback()
-    {
-       registerListener('filterChange',this.handleFilterChange,this)
+    /**
+     * Whether the product tiles are draggable.
+     * TODO - normalize value because it may come as a boolean, string or otherwise.
+     */
+    @api tilesAreDraggable = false;
+
+    /** Current page in the product list. */
+    @track pageNumber = 1;
+
+    /** The number of items on a page. */
+    @track pageSize;
+
+    /** The total number of items matching the selection. */
+    @track totalItemCount = 0;
+
+    /** JSON.stringified version of filters to pass to apex */
+    @track filters = {};
+
+    @wire(CurrentPageReference) pageRef;
+
+    /**
+     * Load the list of available products.
+     */
+    @wire(getProducts, { filters: '$filters', pageNumber: '$pageNumber' })
+    products;
+
+    connectedCallback() {
+        registerListener('filterChange', this.handleFilterChange, this);
     }
 
-    disconnectedCallback()
-    {
+    handleProductSelected(event) {
+        fireEvent(this.pageRef, 'productSelected', event.detail);
+    }
+
+    disconnectedCallback() {
         unregisterAllListeners(this);
     }
 
-    handleFilterChange(filters)
-    {
-        this.filters={...filters};
-        this.pageNumber=1;
+    handleSearchKeyChange(event) {
+        this.filters = {
+            searchKey: event.target.value.toLowerCase()
+        };
+        this.pageNumber = 1;
     }
 
-    handleProductSelected(event)
-    { 
-       fireEvent(this.pageRef,'productSelected',event.detail);
+    handleFilterChange(filters) {
+        this.filters = { ...filters };
+        this.pageNumber = 1;
     }
 
-    handlePreviousPage()
-    {
-       this.pageNumber=this.pageNumber-1;
+    handlePreviousPage() {
+        this.pageNumber = this.pageNumber - 1;
     }
 
-    handleNextPage()
-    {
-        this.pageNumber=this.pageNumber+1;
+    handleNextPage() {
+        this.pageNumber = this.pageNumber + 1;
     }
 }
